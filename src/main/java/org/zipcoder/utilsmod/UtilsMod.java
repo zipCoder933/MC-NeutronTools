d552bb716d1f9638049c91fd4387d9b1bbbcb22d
zipCoder933 <67439766+zipCoder933@users.noreply.github.com>
2025-03-31 12:09:43 -0600

    simplified

--- src/main/java/org/zipcoder/utilsmod/UtilsMod.java
+++ src/main/java/org/zipcoder/utilsmod/UtilsMod.java
@@ -59,30 +59,18 @@ public class UtilsMod {
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
 
-        // Register the item to a creative tab
-        modEventBus.addListener(this::addCreative);
-
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
-
         //Register creative tabs
         TEST_CREATIVE_TABS.register(modEventBus);
     }
 
 
     private void commonSetup(final FMLCommonSetupEvent event) {
-
     }
 
-    // Add the example block item to the building blocks tab
-    private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
-    }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
     @SubscribeEvent
     public void onServerStarting(ServerStartingEvent event) {
-
     }
 
     // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
